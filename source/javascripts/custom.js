83cebc89c0ca22a6ba72411624b0ade2a5a4750a
Masayuki Hatta <mhatta@mhatta.org>
2014-12-31 10:26:23 +0900

    Updated

--- source/javascripts/custom.js
+++ source/javascripts/custom.js
@@ -65,18 +65,25 @@ $(function(){
   });
 
   var githubInfo = $("#gh_repos");
+  if(githubInfo.data('github-user')) {
 
-  github.showRepos({
-    user: githubInfo.data('github-user'),
-    count: githubInfo.data('github-repo-count'),
-    skip_forks: githubInfo.data('github-skip-forks'),
-    target: githubInfo
-  });
+    github.showRepos({
+      user: githubInfo.data('github-user'),
+      count: githubInfo.data('github-repo-count'),
+      skip_forks: githubInfo.data('github-skip-forks'),
+      skip_github_io: githubInfo.data('github-skip-github-io'),
+      target: githubInfo
+    });
+  }
+
 
   var $container = $('#post-container');
   $container.imagesLoaded(function(){
     $container.masonry({
-      itemSelector : '.span4'
+      itemSelector : '.span4',
+      columnWidth: function( containerWidth ) {
+        return containerWidth / 3;
+      }
     });
   });
 
